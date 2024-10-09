3a5f0ee4c90a1401dbe184402a43c07adf821a10
nathzu <nathanielseth.dev@gmail.com>
2024-10-09 17:31:18 +0800

    minor polishing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,12 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 const Contact = () => {
 	return (
 		<div className="flex flex-col justify-center items-center h-screen">
 			<p className="font-light text-center text-xl md:text-xl">
 				Please feel free to reach out,
 				<br />
-				contact me at{" "}
+				email me at{" "}
 				<a
 					href="mailto:nathanielseth.dev@gmail.com"
 					className="text-[#f9364d] font-bold text-2xl"
@@ -13,6 +15,26 @@ const Contact = () => {
 				</a>
 				.
 			</p>
+
+			<div className="flex mt-10 space-x-4">
+				<a
+					href="https://github.com/nathanielseth"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="text-3xl text-white hover:text-[#f9364d]"
+				>
+					<FaGithub />
+				</a>
+
+				<a
+					href="https://www.linkedin.com/in/nathanielseth"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="text-3xl text-white hover:text-blue-600"
+				>
+					<FaLinkedin />
+				</a>
+			</div>
 		</div>
 	);
 };
